7b92e892924a79eae8551b803d1b86e4409bc2d6
bruhoffman <bruhoffman@gmail.com>
2023-11-06 22:20:27 -0300

    Prática 3 da aula Variáveis

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
+const cidade = prompt("Digite a cidade onde mora:");
+let cep = Number(prompt("Digite o CEP da tua cidade:"));
+let turistica = confirm("A cidade é turistica? OK para SIM e Cancelar para NÂO");
 
+console.log("cidade:", typeof cidade, ", cep:", typeof cep, ", turistica:", typeof turistica);
+console.log("A cidade", cidade, "de CEP", cep, "é uma cidade turistica?", turistica);
